d5f01ad10dfd7c84af064bea6a3ffe45c484da1f
umpalumpa0071-cmd <umpalumpa0071@gmail.com>
2026-04-27 08:27:57 +0500

    Update class1.cs

--- task1/lab6/class1.cs
+++ task1/lab6/class1.cs
@@ -1,66 +1,32 @@
-﻿using System;
-
 public class ThreeMarks
 {
-    private int firstMark;
-    private int secondMark;
-    private int thirdMark;
-
     private string comment;
 
-    // конструктор без параметров
-    public ThreeMarks() : this(2, 2, 2, "") { }
+    public ThreeMarks() : this("") { }
 
-    // основной конструктор
-    public ThreeMarks(int a, int b, int c, string comment)
+    public ThreeMarks(string comment)
     {
-        firstMark = a;
-        secondMark = b;
-        thirdMark = c;
         this.comment = comment;
     }
 
-    // конструктор копирования
     public ThreeMarks(ThreeMarks other)
     {
-        firstMark = other.firstMark;
-        secondMark = other.secondMark;
-        thirdMark = other.thirdMark;
         comment = other.comment;
     }
 
-    public int FirstMark
+    public string Comment
     {
-        get { return firstMark; }
-        set { firstMark = value; }
+        get { return comment; }
+        set { comment = value; }
     }
 
-    public int SecondMark
-    {
-        get { return secondMark; }
-        set { secondMark = value; }
-    }
-
-    public int ThirdMark
-    {
-        get { return thirdMark; }
-        set { thirdMark = value; }
-    }
-
-    // метод добавляет !!! к комментарию
     public void AddExclamations()
     {
         comment = "!!!" + comment;
     }
 
-    // средний балл
-    public double GetAverage()
-    {
-        return (firstMark + secondMark + thirdMark) / 3.0;
-    }
-
     public override string ToString()
     {
-        return $"marks: [{firstMark}, {secondMark}, {thirdMark}], comment: {comment}";
+        return $"comment: {comment}";
     }
 }
